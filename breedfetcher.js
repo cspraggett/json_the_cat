0741b7bc4db547d37f0909f0e45552c5c2de000c
Christopher Spraggett <cspraggett@gmail.com>
2020-01-16 13:06:43 -0500

    added error handling and checked for a successful query

--- breedfetcher.js
+++ breedfetcher.js
@@ -2,10 +2,14 @@ const request = require("request");
 const apiAddress = "https://api.thecatapi.com/v1/breeds/search?q=";
 const query = process.argv.slice(2);
 
-console.log(query);
-
 request(`${apiAddress}${query}`, (error, response, body) => {
-  const data = JSON.parse(body);
-  console.log(typeof data);
-  console.log(data[0].description);
+  if (error) {
+    console.error(error);
+  }
+  if (body.length < 3) {
+    console.log("search returned no results");
+  } else {
+    const data = JSON.parse(body);
+    console.log(data[0].description);
+  }
 });
